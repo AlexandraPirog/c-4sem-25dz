dea4bcb133834be4705e5d2143d146a08fe5252e
Alex <119449496+AlexandraPirog@users.noreply.github.com>
2022-12-14 20:12:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
-﻿Console.WriteLine("введите число A");
+/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("введите число A");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число B");
 int b = Convert.ToInt32(Console.ReadLine());
 int stepen = a;
-
+int stepen1 = Auto ();
+int Auto()
+{
 for (int i = 1; i < b; i++)
 {
 stepen = stepen * a;
 }
-Console.WriteLine($"{a} в степени {b} равно: " + stepen);
+return stepen;
+}
+Console.WriteLine($"{a} в степени {b} равно: " + stepen1);
